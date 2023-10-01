f929691c9198755f2a79d34f5ab5fe9159bed6cc
DESKTOP-FIM6H62\USER <hwanghsun@naver.com>
2023-10-01 19:22:20 +0900

    1. QnA 수정

---
+++ DamnVulnerableBank/app/src/main/java/com/app/damnvulnerablebank/QnAWrite.java
@@ -0,0 +1,14 @@
+package com.app.damnvulnerablebank;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QnAWrite extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qna_write);
+    }
+}
